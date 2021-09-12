6bc7ac794af5431cafa6b7ba0f69954094a0d8c4
Chang Chen Chien <e850506@gmail.com>
2021-09-12 16:50:32 +0800

    Tidy up ast package

---
+++ ast/return.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"bytes"
+
+	"github.com/unknowntpo/monkey/token"
+)
+
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
