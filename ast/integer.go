cbd2b32be74a60128238a23107a3a0faa87db8a2
Chang Chen Chien <e850506@gmail.com>
2021-09-13 18:15:39 +0800

    Integer Literal: FAIL
    go test -v ./parser -run=TestIntegerLiteralExpression
    === RUN   TestIntegerLiteralExpression
        parser_test.go:163: exp not *ast.IntegerLiteral. got=<nil>
    --- FAIL: TestIntegerLiteralExpression (0.00s)
    FAIL
    FAIL	github.com/unknowntpo/monkey/parser	0.005s
    FAIL

---
+++ ast/integer.go
@@ -0,0 +1,14 @@
+package ast
+
+import (
+	"github.com/unknowntpo/monkey/token"
+)
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/unknowntpo/monkey/ast"
 	"github.com/unknowntpo/monkey/lexer"
@@ -175,4 +176,19 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
 }
