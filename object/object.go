56b8d01c681284dcc23c5c645d25db9dcbf04ce1
Chang Chen Chien <e850506@gmail.com>
2021-09-29 18:04:10 +0800

    Null

--- object/object.go
+++ object/object.go
@@ -12,6 +12,7 @@ type Object interface {
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Integer struct {
@@ -27,3 +28,8 @@ type Boolean struct {
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
