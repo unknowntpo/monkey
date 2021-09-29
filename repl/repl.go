363e7dfe5c3ef641ec6a92db07c4dee5ff036981
Chang Chen Chien <e850506@gmail.com>
2021-09-29 18:22:51 +0800

    Completing the REPL

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/unknowntpo/monkey/evaluator"
 	"github.com/unknowntpo/monkey/lexer"
 	"github.com/unknowntpo/monkey/parser"
 )
@@ -31,8 +32,14 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
+		//io.WriteString(out, program.String())
+		//io.WriteString(out, "\n")
 	}
 }
 
