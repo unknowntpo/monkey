b2f763d9c70cd27ccca3a422450d49b25572c6d2
Chang Chen Chien <e850506@gmail.com>
2021-10-30 15:17:50 +0800

    3.7 - Return Statements - FAIL
    === RUN   TestReturnStatements
        evaluator_test.go:54: object has wrong value. got=1, want=10
    --- FAIL: TestReturnStatements (0.00s)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -20,6 +20,9 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.BlockStatement:
 		return evalStatements(node.Statements)
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 
 	// Expressions
 	case *ast.IntegerLiteral:
@@ -45,6 +48,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
 	for _, statement := range stmts {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
 	}
 
 	return result
