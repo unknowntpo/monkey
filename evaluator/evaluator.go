ffa3a6db173224d52c8a08fc8e3ce9e91afb0389
Chang Chen Chien <e850506@gmail.com>
2021-10-30 15:49:35 +0800

    3.10 - Functions & Function Calls - PASS

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -71,6 +71,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	// Identifier
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
 	}
 
 	return nil
